1759a4d22996523d55e5b722b04ab2edeb2c77ba
Eduardo De Jesus Oliver Roman <olivereduardoroman@gmail.com>
2023-04-06 14:11:32 -0500

    0.1

---
+++ web_jr_3/modelo/conexion.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$dbname = 'reservaciones';
+$username = 'root';
+$password = '';
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    echo 'Error de conexión: ' . $e->getMessage();
+}
+?>
